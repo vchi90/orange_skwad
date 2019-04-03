9e0bbd77660b553848fcc5b367d2f1886a6af1b9
jiayang <jchen59@stuy.edu>
2019-04-02 22:47:18 -0400

    donut chart

--- static/index.js
+++ static/index.js
@@ -5,14 +5,14 @@ var fill = d3.scaleOrdinal(d3.schemeCategory10);
 var words = "You don't know about me without you have read a book called The Adventures of Tom Sawyer but that ain't no matter.";
 
 //Create the svg element
-var svg = d3.select('.svgcontainer').append('svg').
+var svg0 = d3.select('.svgcontainer').append('svg').
     attr("width",500).
     attr("height", 500).
     append("g")
     .attr("transform","translate(250,250)"); //The cloud initially is centered at 0,0 top left, but we want to center on canvas
 
 function draw(wordlist) {
-    var cloud = svg.selectAll("g text").
+    var cloud = svg0.selectAll("g text").
 	   data(wordlist, function(d) { return d.text; });
     cloud.enter()
     //Format how the words look
@@ -69,42 +69,48 @@ show(); //For some reason, calling it twice is needed because the rotations aren
 
 //======================================DONUT CHART=================SHOULD PROBABLY BE IN ANOTHER FILE
 
-// var width = 260;
-// var height = 260;
-// var thickness = 40;
-// var duration = 750;
-//
-// var radius = width/2;
-// var color = d3.scaleOrdinal(d3.schemeCategory10);
-//
-// var svg = d3.select(".svgcontainer2")
-//     .append('svg')
-//     .attr('class', 'pie')
-//     .attr('width', width)
-//     .attr('height', height)
-//     .append('g')
-//     .attr('transform', 'translate(' + (width/2) + ',' + (height/2) + ')'); //Moving the chart from top left of container to the center
-//
-// //Function to get/display a section of the arc for later
-// var arc = d3.arc()
-//     .innerRadius(radius - thickness)
-//     .outerRadius(radius);
-//
-// var pie = d3.pie()
-//     .value(function(d) { return (Math.random() * 100); }) //For now, assign a random value to the elements (the portion of the whole pie it takes up)
-//     .sort(null); //Show the data by greatest to least
-//
-// svg.selectAll('path')
-//     .data(pie(getWords())) //Enter the data
-//     .enter()
-//     .append("g")
-//     .append('path')
-//     .attr('d', arc) //The arc
-//     .attr('fill', (d,i) => color(i))
-//     .each(function(d, i) { this._current = i; });
-//
-//
-// svg.append('text')
-//   .attr('text-anchor', 'middle')
-//   .attr('dy', '.35em')
-//   .text("Tweets");
+var width = 260;
+var height = 260;
+var thickness = 40;
+var duration = 750;
+
+var radius = width/2;
+var color = d3.scaleOrdinal(d3.schemeCategory10);
+
+var svg1 = d3.select(".svgcontainer2")
+    .append('svg')
+    .attr('class', 'pie')
+    .attr('width', width)
+    .attr('height', height)
+    .append('g')
+    .attr('transform', 'translate(' + (width/2) + ',' + (height/2) + ')'); //Moving the chart from top left of container to the center
+
+//Function to get/display a section of the arc for later
+var arc = d3.arc()
+    .innerRadius(radius - thickness)
+    .outerRadius(radius);
+
+var pie = d3.pie()
+    .value(function(d) { return (Math.random() * 100); }); //For now, assign a random value to the elements (the portion of the whole pie it takes up)
+
+
+svg1.selectAll('path')
+    .data(pie(getWords())) //Enter the data
+    .enter()
+    .append("g")
+    .on("mouseover", function(d) {
+        //console.log("wepy")
+        d3.select(this).append("g").attr("class","text-group") //Each element of the donut chart create a next "textgroup" for accessibility
+            .append("text")
+            .attr("class", "name-text")
+            .text(d.data.text)
+            .attr('text-anchor', 'middle')
+    })
+    .on("mouseout", function(d) {
+        d3.select(this)
+        .select(".text-group").remove(); //Delete the entire text group made above ^^^
+    })
+    .append('path')
+    .attr('d', arc) //The arc
+    .attr('fill', (d,i) => color(i))
+    ;
