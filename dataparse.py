fbdafa0df2c0abbbec3c22b8cf98ca56d1c9e04f
Theodore Peters <tpeters@stuy.edu>
2019-04-04 18:50:06 -0400

    hmm

--- dataparse.py
+++ dataparse.py
@@ -4,6 +4,9 @@ with open("data.csv","r")as f:
     for l in f.read().lower().split("\n"):
         
         w=[w for w in" ".join(w for w in l.split(" ") if w and "http" not in w).replace("."," ").replace("“"," ").replace(":"," ").replace("-"," ").replace("?"," ").replace(","," ").replace(";"," ").replace("\""," ").replace("…"," ").replace("!"," ").replace("—"," ").replace("”"," ").replace("&amp","and").replace("’","'").replace("‘","'").split(" ")if w]
+        for wrd in w:
+            while "\\u" in wrd:
+                wrd = wrd[:wrd.find("\\u")]+" "+wrd[wrd.find("\\u")+6:]
         for i in range(len(w)):
             if w[i] in word:
                 if w[(i+1)%len(w)] in word[w[i]]:
@@ -12,5 +15,5 @@ with open("data.csv","r")as f:
                     word[w[i]][w[(i+1)%len(w)]]=1
             else:
                 word[w[i]]={w[(i+1)%len(w)]:1}
-with open("data/data.json","w") as f:
+with open("static/data.json","w+") as f:
     f.write(json.dumps(word))
