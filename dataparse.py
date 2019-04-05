4a22c60ced8f906f318a86757f5dbfe7d4ee03f6
anotherLostKitten <tpeters@stuy.edu>
2019-04-05 13:02:55 -0400

    ;:;

--- dataparse.py
+++ dataparse.py
@@ -3,10 +3,15 @@ word = {}
 with open("data.csv","r")as f:
     for l in f.read().lower().split("\n"):
         
-        w=[w for w in" ".join(w for w in l.split(" ") if w and "http" not in w).replace("."," ").replace("“"," ").replace(":"," ").replace("-"," ").replace("?"," ").replace(","," ").replace(";"," ").replace("\""," ").replace("…"," ").replace("!"," ").replace("—"," ").replace("”"," ").replace("&amp","and").replace("’","'").replace("‘","'").split(" ")if w]
+        w=[w for w in" ".join(w for w in l.split(" ") if w and "http" not in w).replace("."," ").replace("“"," ").replace(":"," ").replace("-"," ").replace("?"," ").replace(","," ").replace(";"," ").replace("\""," ").replace("…"," ").replace("!"," ").replace("—"," ").replace("”"," ").replace("&amp","and").replace("’","'").replace("‘","'").replace("(","'").replace(")","'").split(" ")if w]
         for wrd in w:
             while "\\u" in wrd:
-                wrd = wrd[:wrd.find("\\u")]+" "+wrd[wrd.find("\\u")+6:]
+                wrd=wrd[:wrd.find("\\u")]+" "+wrd[wrd.find("\\u")+6:]
+            if wrd and wrd[0]=="'":
+                wrd=wrd[1:]
+            if wrd and wrd[-1]=="'":
+                wrd=wrd[:-1]
+        w=[w for w in w if w]
         for i in range(len(w)):
             if w[i] in word:
                 if w[(i+1)%len(w)] in word[w[i]]:
