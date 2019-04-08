481ca259a71580236bc562008e4bfdcb16f85911
anotherLostKitten <tpeters@stuy.edu>
2019-04-08 13:20:20 -0400

    unicode

--- dataparse.py
+++ dataparse.py
@@ -5,15 +5,14 @@ with open("data.csv","r")as f:
 
         w=[w for w in" ".join(w for w in l.split(" ") if w and "http" not in w and "www" not in w).replace("."," ").replace("“"," ").replace(":"," ").replace("?"," ").replace(","," ").replace(";"," ").replace("\""," ").replace("…"," ").replace("!"," ").replace("—"," ").replace("-"," ").replace("”"," ").replace("&amp","and").replace("’","'").replace("‘","'").replace("("," ").replace(")"," ").split(" ")if w]
         for i in range(len(w)):
-
-            while "\\u" in w[i]:
-                w[i]=w[i][:w[i].find("\\u")]+" "+w[i][w[i].find("\\u")+6:]
-                print(w[i])
+            for ch in range(len(w[i])):
+                if ord(w[i][ch])>128:
+                    w[i]=w[i][:ch]+" "+w[i][ch+1:]
             if w[i][0]=="'":
                 w[i]=w[i][1:]
             if w[i] and w[i][-1]=="'":
                 w[i]=w[i][:-1]
-        w=[wr for wr in w if wr]
+        w=[wrr for wrr in" ".join(wr for wr in w if wr).split(" ")if wrr]
         for i in range(len(w)):
             if w[i] in word:
                 if w[(i+1)%len(w)] in word[w[i]]:
